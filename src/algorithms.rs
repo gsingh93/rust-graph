55ac97f26a87409c1554813c970845163581fd0b
Gulshan Singh <gulshan@umich.edu>
2014-08-26 16:31:22 -0400

    Fixed bug in prim's and kruskal's, added tests

--- src/algorithms.rs
+++ src/algorithms.rs
@@ -108,14 +108,18 @@ pub fn prim<V: Clone,
     };
 
     pq.push(PQElt(source, None, None));
-    g.copy_node_to(&mut mst, source);
 
     while mst.size() != g.size() {
+        assert!(!pq.is_empty());
+
         // Pick edge with minimal weight and add to graph
         let PQElt(u, parent, min_edge) = pq.pop().unwrap();
+        if mst.contains_node(u) {
+            continue;
+        }
 
         match (parent, min_edge) {
-            (None, None) => (),
+            (None, None) => g.copy_node_to(&mut mst, source),
             (Some(parent), Some(_)) => {
                 g.copy_edge_to(&mut mst, parent, u);
             },
@@ -136,7 +140,7 @@ pub fn prim<V: Clone,
 
 pub fn kruskal<V: Clone,
                E: Clone + Ord + Weight>(g: &AdjListGraph<V, E>)
-                                        -> AdjListGraph<V, E>{
+                                        -> AdjListGraph<V, E> {
     let mut ds = DisjointSet::new();
     for v in g.nodes_iter() {
         ds.add_set(*v);
@@ -146,7 +150,8 @@ pub fn kruskal<V: Clone,
     for &(u, v) in g.edges_iter() {
         edge_weights.push((u, v, g.edge_prop(u, v)))
     }
-    edge_weights.sort();
+    edge_weights.sort_by(|&(_, _, ref prop1), &(_, _, ref prop2)|
+                         prop2.cmp(prop1));
 
     let mut mst: AdjListGraph<V, E> = AdjListGraph::new(false);
     while !edge_weights.is_empty() {
@@ -193,4 +198,62 @@ pub fn ford_fulkerson() {
 
 pub fn bellman_ford() {
 
+}
+
+#[cfg(test)]
+#[deriving(Clone, Eq, PartialEq, Ord, PartialOrd, Show)]
+struct Edge {
+    weight: int
+}
+
+#[cfg(test)]
+impl Edge {
+    fn new(weight: int) -> Edge {
+        Edge { weight: weight }
+    }
+}
+
+#[cfg(test)]
+impl Weight for Edge {
+    fn weight(&self) -> int {
+        self.weight
+    }
+
+    fn set_weight(&mut self, weight: int) {
+        self.weight = weight;
+    }
+}
+
+#[test]
+fn mst_test() {
+    let mut g: AdjListGraph<(), Edge> = AdjListGraph::new(false);
+    g.add_edge_with_prop(0, 1, Edge::new(4));
+    g.add_edge_with_prop(1, 2, Edge::new(8));
+    g.add_edge_with_prop(2, 3, Edge::new(7));
+    g.add_edge_with_prop(2, 5, Edge::new(5));
+    g.add_edge_with_prop(3, 4, Edge::new(9));
+    g.add_edge_with_prop(3, 5, Edge::new(20));
+    g.add_edge_with_prop(4, 5, Edge::new(19));
+    g.add_edge_with_prop(5, 6, Edge::new(3));
+    g.add_edge_with_prop(6, 7, Edge::new(1));
+    g.add_edge_with_prop(7, 8, Edge::new(18));
+    g.add_edge_with_prop(6, 8, Edge::new(17));
+    g.add_edge_with_prop(8, 2, Edge::new(2));
+    g.add_edge_with_prop(7, 0, Edge::new(16));
+    g.add_edge_with_prop(1, 7, Edge::new(11));
+
+    let mut mst: AdjListGraph<(), Edge> = AdjListGraph::new(false);
+    mst.add_edge_with_prop(0, 1, Edge::new(4));
+    mst.add_edge_with_prop(2, 3, Edge::new(7));
+    mst.add_edge_with_prop(2, 5, Edge::new(5));
+    mst.add_edge_with_prop(3, 4, Edge::new(9));
+    mst.add_edge_with_prop(5, 6, Edge::new(3));
+    mst.add_edge_with_prop(6, 7, Edge::new(1));
+    mst.add_edge_with_prop(8, 2, Edge::new(2));
+    mst.add_edge_with_prop(1, 2, Edge::new(8));
+
+    let pmst = prim(&g);
+    let kmst = kruskal(&g);
+    assert_eq!(mst, pmst);
+    assert_eq!(mst, kmst);
 }
